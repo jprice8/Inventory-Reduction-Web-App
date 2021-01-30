31589c30cddfc915557f2def74331d24a93fa7f4
Jackson Price <jdprice1995@gmail.com>
2021-01-29 21:11:08 -0600

    modified settings for deployment.'

--- reductionapp/settings.py
+++ reductionapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import json
+import logging.config
 
 import sec_vars
 
@@ -25,12 +27,12 @@ ACT_TEMPLATE_DIR = os.path.join(BASE_DIR, "act", "templates")
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = sec_vars.SECRET_KEY
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
@@ -48,7 +50,7 @@ INSTALLED_APPS = [
     'target',
     'act',
     'django.contrib.humanize',
-    # 'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -92,12 +94,17 @@ WSGI_APPLICATION = 'reductionapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'reductionapp',
-        'USER': sec_vars.DB_OWNER,
-        'PASSWORD': sec_vars.DB_PW,
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.{}'.format(
+            os.getenv('DATABASE_ENGINE', 'sqlite3')
+        ),
+        'NAME': os.getenv('DATABASE_USERNAME', 'reductionapp'),
+        'USER': os.getenv('DATABASE_USER', 'jacksonprice'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'Space1234'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
+        'OPTIONS': json.loads(
+            os.getenv('DATABASE_OPTIONS', '{}')
+        ),
     }
 }
 
@@ -138,8 +145,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "inventory", "static"),
     os.path.join(BASE_DIR, "target", "static"),
@@ -157,3 +162,39 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = sec_vars.EMAIL_HOST_UN
 EMAIL_HOST_PASSWORD = sec_vars.EMAIL_HOST_PW
+
+# django storages module moving static assets to digital ocean spaces
+AWS_ACCESS_KEY_ID = os.getenv('STATIC_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('STATIC_SECRET_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('STATIC_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = os.getenv('STATIC_ENDPOINT_URL')
+AWS_S3_CUSTOM_DOMAIN = os.getenv('STATIC_CUSTOM_ENDPOINT_URL')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+AWS_DEFAULT_ACL = 'public-read'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+
+STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_ROOT = 'static/'
+
+# logging config
+
+# clear prev config
+LOGGING_CONFIG = None
+
+# get loglevel from env
+LOGLEVEL = os.getenv('DJANGO_LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
+})
