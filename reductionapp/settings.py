974db28a1a43c8d6d6ab603bca7e001e9f724678
Jackson Price <jdprice1995@gmail.com>
2021-02-01 15:43:06 -0600

    Edit allowed hosts.

--- reductionapp/settings.py
+++ reductionapp/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '18.218.0.219']
 
 # Application definition
 
